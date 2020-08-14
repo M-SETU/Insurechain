3f83107d92d395fc1635bd82e23cb02e751a19c2
nitinmittal23 <nitinmittal44066@gmail.com>
2020-08-14 18:59:24 +0530

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { Button, Card, Image } from 'semantic-ui-react'
 import { Redirect, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import CreatePolicy from './CreatePolicy.js';
+import logo from '../images/logos/Matic logo symbol.png';
 
 class Home extends Component {
 
@@ -43,44 +45,59 @@ class Home extends Component {
     }
 
     return (
-      <div style={{
-        margin: "20px"
-      }}>
-          <Card.Group>
-          <Card>
-            <Card.Content>
-              <Card.Header>Vendor 1</Card.Header>
-              <Card.Meta>LC</Card.Meta>
-              <Card.Description>
-              <strong>LIC</strong>
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <div className='ui two buttons'>
-                <Button onClick={this.handleSubmit1} basic color='green'>
-                  Select
-                </Button>
-            
-              </div>
-            </Card.Content>
-          </Card>
-          <Card>
-            <Card.Content>
-              <Card.Header>Vendor 2</Card.Header>
-              <Card.Meta>HF</Card.Meta>
-              <Card.Description>
-                HDFC
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <div className='ui two buttons'>
-                <Button onClick={this.handleSubmit2} basic color='green'>
-                  Select
-                </Button>
-              </div>
-            </Card.Content>
-          </Card>
-        </Card.Group>
+      <div>
+        <nav className="navbar navbar-light" style={{backgroundColor:"#0B1647"}}>
+          <a className="navbar-brand">
+            <img src={logo} style = {{width: "60px" , height: "60px"}} />
+            <strong style={{ display: "inline-block" , paddingtop: "20px", color:"white" ,fontSize:"37px"}}>Matic</strong>
+          </a>
+        </nav>
+
+
+      
+        <div align="center">
+          <div style={{margin: "20px", display: "inline-block"}}>
+            <div align= "left">
+              <Card.Group>
+              <Card>
+                <Card.Content>
+                  <Card.Header>Vendor 1</Card.Header>
+                  <Card.Meta>LC</Card.Meta>
+                  <Card.Description>
+                    <strong>LIC</strong><br></br>
+                    <strong>Goerli</strong>
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <div className='ui two buttons'>
+                    <Button onClick={this.handleSubmit1} basic color='blue'>
+                      Select
+                    </Button>
+                
+                  </div>
+                </Card.Content>
+              </Card>
+              <Card>
+                <Card.Content>
+                  <Card.Header>Vendor 2</Card.Header>
+                  <Card.Meta>HF</Card.Meta>
+                  <Card.Description>
+                    <strong>HDFC</strong><br></br>
+                    <strong>Matic</strong>
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <div className='ui two buttons'>
+                    <Button onClick={this.handleSubmit2} basic color='blue'>
+                      Select
+                    </Button>
+                  </div>
+                </Card.Content>
+              </Card>
+            </Card.Group>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
