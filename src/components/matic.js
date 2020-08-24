075c954925658d76230019563917577b06a24298
Aveesh Shetty <aveeshshetty@Aveeshs-MacBook-Air.local>
2020-08-25 00:05:35 +0530

    portis with porting

--- src/components/matic.js
+++ src/components/matic.js
@@ -19,13 +19,18 @@ class Pos extends Component {
           account: '',
           web3: {},
         };
-        this.loadBlockchainData = this.loadBlockchainData.bind(this);
+        this.depositer = this.depositer.bind(this);
+        this.withdrawer = this.withdrawer.bind(this);
+        this.burned = this.burned.bind(this);
+        this.exiter = this.exiter.bind(this);
+        this.getMaticClient = this.getMaticClient.bind(this);
+        this.port = this.port.bind(this);
       
     }
   
     async componentWillMount() {
         await this.loadWeb3()
-        await this.loadBlockchainData()
+       
 
     }
 
@@ -43,49 +48,31 @@ class Pos extends Component {
             portisGoerli: portisGoerli
         })
         const web3 = new Web3(portisMumbai.provider);
+        
         this.setState({ 
             web3: web3 })
         let acc = await web3.eth.getAccounts();
+        
         this.setState({
             account: acc[0]
         })
+        console.log(this.state.account);
 
-        // if (window.ethereum) {
-        // window.web3 = new Web3(window.ethereum)
-        // await window.ethereum.enable()
-        // }
-        // else if (window.web3) {
-        // window.web3 = new Web3(window.web3.currentProvider)
-        // }
-        // else {
-        // window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-        // }
 
-    }
+        console.log(this.state.portisGoerli)
+    
+
+    }                                                               
   
 
-    async loadBlockchainData() {
-        // const web3 = window.web3
-
-        // const accounts = await web3.eth.getAccounts()
-        // this.setState({ account: accounts[0] })
-        
-
-        // const ethBalance = await web3.eth.getBalance(this.state.account)
-        // this.setState({ ethBalance })
-        // console.log(ethBalance);
-
-        // Load Token
-        //const networkId =  await web3.eth.net.getId()
-
-  }
+    
   
     async getMaticClient(_network = "testnet", _version = "mumbai") {
         console.log({
             network: _network,
             version: _version,
-            parentProvider: this.state.portisMumbai.provider,
-            maticProvider: this.state.portisGoerli.provider,  
+            parentProvider: this.state.portisGoerli.provider,
+            maticProvider: this.state.portisMumbai.provider,  
             parentDefaultOptions:  this.state.account , 
             maticDefaultOptions: this.state.account,
         }); 
@@ -94,40 +81,57 @@ class Pos extends Component {
         const matic = new Matic({
             network: _network,
             version: _version,
-            parentProvider: this.state.portisMumbai.provider,
-            maticProvider: this.state.portisGoerli.provider,  
+            parentProvider: this.state.portisGoerli.provider,
+            maticProvider: this.state.portisMumbai.provider,  
             parentDefaultOptions:  this.state.account , 
             maticDefaultOptions: this.state.account,
         });
-        await matic.initialize();
-
+        await matic.initialize();       
+        
         return { matic, network };
     }
 
 
-    async execute() {
+
+   
+
+    async burned() {
+        
+        console.log('asaa');
         const { matic, network } = await this.getMaticClient();
+     
         console.log(matic);
         console.log(network);
         // burning erc721 tokens are also supported
         //Matic chain address
-        const token = "0x6E441c6E95387c6761Ec44CB006891583c4Bed06";
-        console.log(token);
+         //const token = "0x6E441c6E95387c6761Ec44CB006891583c4Bed06";
+          const token = "0x33FC58F12A56280503b04AC7911D1EceEBcE179c";
+        console.log(this.state.account);
+    
     
         // or provide the tokenId in case of an erc721
-        const tokenId = "39713";
-        const hash = await matic.startWithdrawForNFT(token, tokenId, {from: this.state.account});
+        const tokenId = "78840842";
+        const hash = await matic.startWithdrawForNFT(token, tokenId, { from:this.state.account});
         await this.setState({
             hash: hash.transactionHash
         });
         console.log(hash.transactionHash);
         console.log(this.state.hash);
         localStorage.setItem('hash',this.state.hash); 
-        
+        setTimeout(this.port(), 1200000);   
         
     }
 
-    async execute1() {
+
+  async port(){
+   
+    await this.withdrawer();
+    await this.exiter();
+    await this.depositer();
+
+ }   
+
+    async withdrawer() {
         const hash1 = localStorage.getItem('hash');
         console.log(hash1);  
         const { matic, network } = await this.getMaticClient();
@@ -141,24 +145,24 @@ class Pos extends Component {
 
   // Withdraw process is completed, funds will be transfered to your account after challege period is over.
 
-    async execute2() {
+    async exiter() {
         const { matic, network } = await this.getMaticClient();
         
         //Goerli chain address
-        const token = "0x1C250633D9e29a2353dE9FF04aBd8bBb316eE4c5";
+        const token = "0xfA08B72137eF907dEB3F202a60EfBc610D2f224b";
         const phash = await matic.processExits(token, { from: this.state.account, gas: 7000000 })
         console.log(phash.transactionHash);
         localStorage.setItem('phash',phash.transactionHash);
     }
   
-    async execute3() {
+    async depositer(){
         const { matic, network } = await this.getMaticClient();
-    
-        const token = "0x1C250633D9e29a2353dE9FF04aBd8bBb316eE4c5";
-        const tokenId = "39713";
+        console.log(this.state.account)
+        const token = "0xfA08B72137eF907dEB3F202a60EfBc610D2f224b";
+        const tokenId = "676";
     
         
-        await matic.approveERC20TokensForDeposit(token, tokenId).then((res) => {
+        await matic.approveERC20TokensForDeposit(token, tokenId,{ from: this.state.account }).then((res) => {
         console.log("approve hash: ", res.transactionHash);
         });
     
@@ -171,16 +175,15 @@ class Pos extends Component {
     render() {
         return (
         <div>
-            <button onClick={() => this.execute()}>Burn</button><br></br>
+            <button onClick={() => this.burned()}>PORT</button><br></br>
             <h1>{this.state.hash}</h1>
             <a href={`https://mumbai-explorer.matic.today/tx/${localStorage.getItem('hash')}/token_transfers`} target="_blank">Hash</a><br></br>
-            <button onClick={() => this.execute1()}>Confirm Withdraw</button><br></br>
+           
             <a href={`https://goerli.etherscan.io/tx/${localStorage.getItem('chash')}`} target="_blank">Hash</a><br></br>
-            
-            <button onClick={() => this.execute2()}>Process Exit</button><br></br>
+        
             <a href={`https://goerli.etherscan.io/tx/${localStorage.getItem('phash')}`} target="_blank">Hash</a><br></br>
             
-            <button onClick={() => this.execute3()}>Deposit</button><br></br>
+        
             <a href={`https://goerli.etherscan.io/tx/${localStorage.getItem('dhash')}`}  target="_blank">Hash</a><br></br>
             
         </div>
