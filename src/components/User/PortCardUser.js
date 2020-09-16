1fc6f9084e320f2ee1c90a0bc626e08460e5ece2
nitinmittal23 <nitinmittal44066@gmail.com>
2020-09-16 22:35:25 +0530

    Status

--- src/components/User/PortCardUser.js
+++ src/components/User/PortCardUser.js
@@ -8,12 +8,17 @@ const PortCardUser = props => {
         "approved": false,
         "completed": true
     }
+    const statusMessage = {
+        "active": "Waiting for " + props.vendorMapping[props.portCard[3]] + " to Approve or Reject",
+        "approved": "Waiting to Initiate Transfer",
+        "completed": "Awaiting OnBoard by " + props.vendorMapping[props.portCard[3]]
+    }
         
     return(
         <tr>
             <td data-label="policyID" style={{textAlign:"center"}}>{props.portCard[0]}</td>
             <td data-label="vendor" style={{textAlign:"center"}}>{props.vendorMapping[props.portCard[3]]}</td>
-            <td data-label="status" style={{textAlign:"center"}}>{props.portCard[5]}</td>
+            <td data-label="status" style={{textAlign:"center"}}>{statusMessage[props.portCard[5]]}</td>
             <td style={{textAlign:"center"}}>
                 <Button 
                     onClick={() => {props.handleTransferButton(props.portCard[0])}} 
